8ea8588f03c53dc5f0dd1d536b9af8204164f8ce
NoahAbc12345 <63483138+NoahAbc12345@users.noreply.github.com>
2020-07-11 19:10:53 -0600

    Attempt at fixing multiple invalid permission messages.

--- modules/errors.py
+++ modules/errors.py
@@ -6,9 +6,16 @@ class Errors(commands.Cog):
 
     @commands.Cog.listener() # Ignore wrong commands.
     async def on_command_error(self, ctx, error):
-        if isinstance(error, commands.CommandNotFound): pass
-        if isinstance(error, commands.CommandOnCooldown): await ctx.send(f"{ctx.author.mention}, this command is on cooldown. Please try again in {error.retry_after:.0f} seconds.")
-        if isinstance(error, commands.MissingPermissions): await ctx.send(f"{ctx.author.mention}, you don't have permission to use this command!")
-        if isinstance(error, commands.CheckFailure): await ctx.send(f"{ctx.author.mention}, you don't have permission to use this command!")
+        if isinstance(error, commands.CommandNotFound):
+            pass
+        if isinstance(error, commands.CommandOnCooldown):
+            await ctx.send(f"{ctx.author.mention}, this command is on cooldown. Please try again in {error.retry_after:.0f} seconds.")
+            return
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send(f"{ctx.author.mention}, you don't have permission to use this command!")
+            return
+        if isinstance(error, commands.CheckFailure):
+            await ctx.send(f"{ctx.author.mention}, you don't have permission to use this command!")
+            return
 
 def setup(bot): bot.add_cog(Errors(bot))
