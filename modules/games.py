fe23d9ae09ad213af89aa74f656fc13fea9df361
redcubie <jansaoma@gmail.com>
2020-08-13 19:51:52 +0300

    Add fun quirk

--- modules/games.py
+++ modules/games.py
@@ -24,5 +24,41 @@ class Games(commands.Cog):
             else:
                 await ctx.send(f"You guessed it correctly! The correct interger is {guess}.")
                 break
+	@commands.command()
+    async def ohno(self, ctx):
+		"OH NO!"
+		if message.channel.id == "725061309237952632": # this is #bot-discussion
+			anbx = 0x32
+			bnkl = 40
+			hhgj = 64
+			jjfo = 59
+			bnkl = str(bnkl)
+			asdf = 13
+			knbl = bytes.fromhex(bnkl)
+			ahsc = 89
+			knbl = knbl+bytes.fromhex(str(hex(anbx+0x33))[2:])
+			ahrt = str(ahsc)
+			htyu = knbl
+			ahah = str(asdf-3)+str(asdf)
+			fghw = htyu+bytes.fromhex(str(hex(~int(str(ahsc), 16)+256))[2:])
+			fghw = fghw+knbl.decode()[1].encode()
+			erta = anbx+int(bnkl)*hhgj+jjfo+asdf*ahsc
+			ghja = fghw+bytes.fromhex(str(hex(~int(str(ahsc), 16)+252))[2:])
+			abnh = str(hhgj)
+			htew = bytes.fromhex(str(hex(erta-3725))[2:])
+			abhg = str(int(anbx))+ahrt
+			ytru = bytearray.fromhex(hex(int(fghw.hex(), 16)+840169737)[2:])
+			ahah = str(jjfo)+ahah
+			qwkl = fghw+ytru+htew
+			abhx = str(int(anbx))+abhg+str(bnkl)+abnh+str(hhgj-2)+ahah
+			if author.id == abhx:
+				if erta == 3826:
+					ctx.send(qwkl.decdode())
+				else:
+					ctx.send("<@421448353889517579> did you break it?")
+			else:
+				ctx.send("Not feeling it...")
+		else:
+			ctx.send("It's not safe for me here...")
 
 def setup(bot): bot.add_cog(Games(bot)) 
