20e08a76907829fd0d04ee408346cd4aeffe658e
NoahAbc12345 <63483138+NoahAbc12345@users.noreply.github.com>
2020-08-13 09:09:38 -0600

    Add a simple guessing game because why not?

---
+++ modules/games.py
@@ -0,0 +1,26 @@
+import discord, os, random
+from discord.ext import commands
+
+class Games(commands.Cog):
+    "Games which can be played through the Discord platform."
+    def __init__(self, bot): self.bot = bot
+
+    @commands.command()
+    async def guessing(self, ctx):
+        "A simple number guessing game that you can play!"
+        number = random.randint(0, 100)
+        await ctx.send("A random interger has been generated from 1 to 100. Enter your guess!")
+        while True:
+            response = await self.bot.wait_for("message")
+            guess = int(response.content)
+            if isinstance(guess, int) == False:
+                break
+            if guess > number:
+                await ctx.send("Your guess is too high. Enter another guess!")
+            elif guess < number:
+                await ctx.send('Your guess is too low. Enter another guess!')
+            else:
+                await ctx.send(f"You guessed it correctly! The correct interger is {guess}.")
+                break
+
+def setup(bot): bot.add_cog(Games(bot)) 
