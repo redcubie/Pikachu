9aa3bff1c574b2c1ddb28efe241b8cb329cdd1d2
NoahAbc12345 <63483138+NoahAbc12345@users.noreply.github.com>
2020-11-03 15:56:44 -0700

    Remove copyright notice and adjust default help command.

--- manager.py
+++ manager.py
@@ -1,23 +1,10 @@
-# Copyright Notice:
-# Pikachu, a utility bot for the Nincord server.
-# Copyright (C) 2020 NoahAbc12345
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <https://www.gnu.org/licenses/>.
-
 import discord, os, importlib
 from discord.ext import commands
 import configuration.variables as variables
 import configuration.arrays as arrays
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix=("P!", "p!"), case_insensitive=True, allowed_mentions=discord.AllowedMentions.none(), intents=intents)
+bot.help_command = commands.DefaultHelpCommand(no_category="Other")
 
 def is_staff_member(): # The staff member check.
     async def predicate(ctx):
