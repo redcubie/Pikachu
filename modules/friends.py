f8b1eb1820f741c89f57dde31c8c85f92cc10a8c
NoahAbc12345 <63483138+NoahAbc12345@users.noreply.github.com>
2020-11-28 12:48:38 -0700

    Adjust validity checks in friends module.

--- modules/friends.py
+++ modules/friends.py
@@ -13,35 +13,26 @@ class Friends(commands.Cog):
         "Links a friend code or username to the database.\nValid argument options are \"3ds\", \"switch\", \"nx\", and \"wiiu\".\nLinked accounts can include \"Nintendo Switch\", \"Nintendo 3DS\", and \"Nintendo Wii U\"."
         cluster = MongoClient(variables.DBACCOUNT)
         database = cluster["Friends"]
-        async def checkIDWiiU(code):
-            if len(code) < 6 or len(code) > 16: return False
-            if code.startswith((".", "!", "_")) or code.endswith((".", "!", "_")): return False
-            else: return True
         async def checkID3DS(code):
-            if len(code) != 12 and len(code) != 14: return False
-            if len(code) == 12 and code.isdigit() == False: return False
-            if len(code) == 14 and (code[4] != "-" or code[9] != "-"): return False
-            if re.search('[a-zA-Z]', code): return False
-            else: return True
+            return (True if re.match(r"^(\d{4}-\d{4}-\d{4}|\d{12})$", code) else False)
         async def checkIDSwitch(code):
-            if len(code) != 12 and len(code) != 14 and len(code) != 17: return False
-            if len(code) == 12 and code.isdigit() == False: return False
-            if len(code) == 14 and code.startswith("SW") == False and (code[4] != "-" or code[9] != "-"): return False
-            if len(code) == 17 and code.startswith("SW") and (code[3] != "-" or code[8] != "0" or code[13] != "-"): return False
-            else: return True
+            return (True if re.match(r"^(SW-\d{4}-\d{4}-\d{4}|SW\d{12}|\d{4}-\d{4}-\d{4}|\d{12})$", code) else False)
+        async def checkIDWiiU(code):
+            if len(code) < 6 or len(code) > 16 or re.search(r"[^a-zA-Z\d\.\-\_]", code): return False
+            return (False if re.search(r"^[\.\-\_]|[\.\-\_]$", code) or re.search(r"[\.\-\_][\.\-\_]", code) else True)
         if system.lower() == "wiiu":
             collection = database["Nintendo Wii U"]
-            if await checkIDWiiU(code) == False: return await ctx.send(f"{ctx.author.mention}, the account you have provided is invalid.")
+            if not await checkIDWiiU(code): return await ctx.send(f"{ctx.author.mention}, the account you have provided is invalid.")
         elif system.lower() == "3ds":
             collection = database["Nintendo 3DS"]
-            if await checkID3DS(code) == False: return await ctx.send(f"{ctx.author.mention}, the account you have provided is invalid.")
+            if not await checkID3DS(code): return await ctx.send(f"{ctx.author.mention}, the account you have provided is invalid.")
             if len(code) == 12: code = "-".join((code[0:4], code[4:8], code[8:12]))
         elif system.lower() == "switch" or system.lower() == "nx":
             collection = database["Nintendo Switch"]
-            if await checkIDSwitch(code) == False: return await ctx.send(f"{ctx.author.mention}, the account you have provided is invalid.")
+            if not await checkIDSwitch(code): return await ctx.send(f"{ctx.author.mention}, the account you have provided is invalid.")
             if len(code) == 12: code = "-".join(("SW", code[0:4], code[4:8], code[8:12]))
             elif len(code) == 14 and code.startswith("SW"): code = "-".join((code[0:2], code[2:6], code[6:10], code[10:14]))
-            elif len(code) == 14 and code.startswith("SW") == False: code = "SW-" + code
+            elif len(code) == 14 and not code.startswith("SW"): code = "SW-" + code
         else: await ctx.send(f"{ctx.author.mention}, the system you have specified is not supported.")
         if collection.count_documents({"_id": ctx.author.id}, limit = 1) == 0:
             post = {"_id": ctx.author.id, "Account": None}
@@ -79,19 +70,19 @@ class Friends(commands.Cog):
             accountCounter = accountCounter + 1
             results = collection.find({"_id": member.id})
             for result in results: accountCode = result["Account"]
-            embed.add_field(name="Nintendo 3DS", value=accountCode, inline=False)
+            embed.add_field(name="Nintendo 3DS (Friend Code)", value=accountCode, inline=False)
         collection = database["Nintendo Switch"]
         if collection.count_documents({"_id": member.id}, limit = 1) == 1:
             accountCounter = accountCounter + 1
             results = collection.find({"_id": member.id})
             for result in results: accountCode = result["Account"]
-            embed.add_field(name="Nintendo Switch", value=accountCode, inline=False)
+            embed.add_field(name="Nintendo Switch (Friend Code)", value=accountCode, inline=False)
         collection = database["Nintendo Wii U"]
         if collection.count_documents({"_id": member.id}, limit = 1) == 1:
             accountCounter = accountCounter + 1
             results = collection.find({"_id": member.id})
             for result in results: accountCode = result["Account"]
-            embed.add_field(name="Nintendo Wii U", value=accountCode, inline=False)
+            embed.add_field(name="Nintendo Wii U (Username)", value=accountCode, inline=False)
         embed.set_footer(text=f"Update accounts with p!accountlink.")
         if accountCounter > 0 and member == ctx.author: return await ctx.send(f"{ctx.author.mention}, here's your list of linked accounts.", embed=embed)
         elif accountCounter > 0 and member != ctx.author: return await ctx.send(f"{ctx.author.mention}, here's the list of linked accounts for {member.mention}.", embed=embed)
